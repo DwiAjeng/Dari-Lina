6d8b64206b04f37266959fc370ac61f3752b7d8a
Linakartika <linakartikasari97@gmail.com>
2017-04-05 09:33:49 +0700

    MODIFIKASI

--- lina/src/com/company/Main.java
+++ lina/src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[]args) {
         Scanner show = new Scanner(System.in);
         System.out.print("untuk nomor 1c (modifikasi) ");
+        System.out.print("Ini yang bener untuk nomor 1c (modifikasi) FIX");
         System.out.print("Masukan Jumlah Deret Fibonacci ");
         int n = show.nextInt();
         long fib[] = new long[n];
